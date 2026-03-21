43bb57f9a280aebde97f2964a8a0c9623f1bf043
Aathithya-J <87900917+Aathithya-J@users.noreply.github.com>
2026-03-21 03:28:22 -0400

    Fix: Set sign_locations to safe bottom-right (0.65, 0.1)

--- api/sign.js
+++ api/sign.js
@@ -106,8 +106,8 @@ bQotHZrdaiEpoWTtcaE/jxqjhU8t0pY6Yy7PFGY7l0jCFTOwtIj6pC50
       sign_locations: [
         {
           page: 1,
-          x: 0.72,
-          y: 0.05,
+          x: 0.65,
+          y: 0.1,
           width: 0.25,
           height: 0.06
         }
